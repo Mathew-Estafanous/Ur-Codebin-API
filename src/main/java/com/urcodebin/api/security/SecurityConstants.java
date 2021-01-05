89d79ce5b490f40b88578af11c0930d04670ba8b
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2021-01-04 19:07:53 -0500

    Add comments explaining reasoning for getSecret() and approach taken.

--- src/main/java/com/urcodebin/api/security/SecurityConstants.java
+++ src/main/java/com/urcodebin/api/security/SecurityConstants.java
@@ -18,6 +18,14 @@ public class SecurityConstants {
     public static final String SIGN_UP_URL = "/api/account/signup";
     public static final String PUBLIC_PASTE_URL = "/api/paste/public";
 
+    /**
+     * The secret is not stored in plain text as it would raise security issues
+     * as it will be publicly available in the open-source project. To ensure
+     * security, the {@key SECRET} is encoded in a .properties file and is then
+     * decoded using Jasypt String Encryptor. This process requires a
+     * password which is safely stored as an environment variable called
+     * {@env JASYPT_ENCRYPTOR_PASSWORD}
+     */
     public static String getSecret() {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         //We need the Jasypt password that is used to decrypt the SECRET property.
